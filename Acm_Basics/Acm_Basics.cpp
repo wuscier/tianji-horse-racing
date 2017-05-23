6766f6cd7ee78f4970ee3aaa91dd5b025c8f1c26
big cash <1352288123@qq.com>
2017-05-23 16:04:26 +0800

    add project for solving problem1.1.1

---
+++ Acm_Basics/Acm_Basics.cpp
@@ -0,0 +1,21 @@
+// by bigcash, 2017-05-23 15:27:18
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+
+
+	//#pragma region No1
+	//int a, b;
+	//while (scanf("%d%d", &a, &b) != EOF)
+	//{
+	//	printf("%d\n", a + b);
+	//}
+	//return 0;
+
+	//#pragma endregion
+}
+
