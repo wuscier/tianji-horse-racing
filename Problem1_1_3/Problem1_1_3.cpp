09028224e85a5ba18ee28ea91f386b228f405cc5
big cash <1352288123@qq.com>
2017-05-23 16:46:20 +0800

    add projects for solving problem1_1_2 and problem1_1_3

---
+++ Problem1_1_3/Problem1_1_3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+	int a, b;
+
+	while (scanf("%d%d", &a, &b) && !(a == 0 && b == 0))
+	{
+		printf("%d\n", a + b);
+	}
+
+	return 0;
+}
